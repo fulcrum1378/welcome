c5fefefc813f3b2fc71ceafe99ec2e2ff0b5b63a
Mahdi Parastesh <fulcrum1378@gmail.com>
2021-11-09 20:19:00 +0000

    2021.11.09 - 3

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ try:
     for g in cgi.FieldStorage().list: got[g.name] = g.value
 except:
     pass
-lang = got["hl"]
+l = got["hl"]
 print("Content-type: text/html\n")
 print(temp["temp.html"](**{
     "root": "/" + folder + "/",
@@ -18,11 +18,11 @@ print(temp["temp.html"](**{
     "title": {
         "en": "Mahdi Parastesh",
         "fa": "مهدی پرستش"
-    },
+    }[l],
     "header": {
         "en": "Mahdi Parastesh",
         "fa": "مهدی پرستش"
-    },
+    }[l],
     "biography": {
         "en": """I have 6 years of experience in <b>Full-Stack software engineering</b>.<br><br>
     The programming languages I'm skilled in are JavaScript, PHP, Java, SQL, Kotlin, Dart, Python and Prolog.
@@ -30,7 +30,7 @@ print(temp["temp.html"](**{
     I also know Node.js, TensorFlow, PyTorch, MySQL and PostgreSQL.<br>
     I practice Machine Learning and I'm also an expert in Linux.""",
         "fa": """"""
-    },
+    }[l],
     "social": [
         {
             "icon": "github",
@@ -69,11 +69,11 @@ print(temp["temp.html"](**{
             "name": {
                 "en": "Mergen",
                 "fa": "مِرگِن"
-            },
+            }[l],
             "desc": {
                 "en": "A logical Artificial Intelligence software, an operating system for a robot.",
                 "fa": "یک نرم افزار هوش مصنوعی منطقی، سیستم عاملی برای یک ربات."
-            },
+            }[l],
             "source": "https://github.com/fulcrum1378/mergen_android",
         },
         # {
@@ -87,11 +87,11 @@ print(temp["temp.html"](**{
             "name": {
                 "en": "Telexporter",
                 "fa": "تلکسپورتر"
-            },
+            }[l],
             "desc": {
                 "en": "Export your messages and call history to HTML, PDF or JSON files.",
                 "fa": "اس ام اس ها و تاریخچه تماس های خود را در قالب های وب، پی دی اف و جیسون استخراج کنید."
-            },
+            }[l],
             "source": "https://github.com/fulcrum1378/telexporter",
         },
         {
@@ -99,11 +99,11 @@ print(temp["temp.html"](**{
             "name": {
                 "en": "Migratio",
                 "fa": "میگراتیو"
-            },
+            }[l],
             "desc": {
                 "en": "Migratio Wordpress-powered website",
                 "fa": "وبسایت وردپرسی میگراتیو"
-            },
+            }[l],
             "source": "https://github.com/fulcrum1378/migratio",
         },
         {
@@ -111,11 +111,11 @@ print(temp["temp.html"](**{
             "name": {
                 "en": "Migratio (Android)",
                 "fa": "میگراتیو (اندروید)"
-            },
+            }[l],
             "desc": {
                 "en": "A geographical statistical tool for determining someone's best destination for migration",
                 "fa": "یک ابزار جغرافی-آماری برای تعیین کردن بهترین مقصد مهاجرت افراد مختلف"
-            },
+            }[l],
             "source": "https://github.com/fulcrum1378/migratio_android",
             "target": "https://migratio.mahdiparastesh.ir/",
         },
@@ -124,11 +124,11 @@ print(temp["temp.html"](**{
             "name": {
                 "en": "Sexbook",
                 "fa": "سکسبوک"
-            },
+            }[l],
             "desc": {
                 "en": "Control your sexual life easily",
                 "fa": "زندگی جنسی خود را به آسانی کنترل کنید"
-            },
+            }[l],
             "source": "https://github.com/fulcrum1378/sexbook",
         },
         {
@@ -136,11 +136,11 @@ print(temp["temp.html"](**{
             "name": {
                 "en": "Friend Tracker",
                 "fa": "ردیاب رفقا"
-            },
+            }[l],
             "desc": {
                 "en": "Easily track your friends on the map.",
                 "fa": "رفقای خود را از روی نقشه ردیابی نمایید."
-            },
+            }[l],
             "source": "https://github.com/fulcrum1378/friend_tracker"
         },
         {
@@ -148,12 +148,16 @@ print(temp["temp.html"](**{
             "name": {
                 "en": "Saam",
                 "fa": "سام"
-            },
+            }[l],
             "desc": {
                 "en": "Stock market data collector based on MetaTrader5",
                 "fa": "گرداور و ذخیره کننده اطلاعات بورس، ساخته شده بر مبنای متاتریدر 5"
-            },
+            }[l],
             "source": "https://github.com/fulcrum1378/saam",
         },
     ],
 }))
+
+# HTML Notes:
+# <object data="${root}img/${s.icon}.svg" type="image/svg+xml"></object>
+# Animated button must be put in an inner element; so that it won't drop out of its bounds!
