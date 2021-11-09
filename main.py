63d4a255711bdd5634b458bae0e70aa55ae3225f
Mahdi Parastesh <fulcrum1378@gmail.com>
2021-11-09 23:21:13 +0330

    2021.11.09 - 1

--- main.py
+++ main.py
@@ -4,46 +4,61 @@ from chameleon import PageTemplateFile, PageTemplateLoader
 
 folder = "welcome"
 temp = PageTemplateLoader(os.path.join(os.path.dirname(__file__), folder))
+got = {"hl": "en"}
+try:
+    for g in cgi.FieldStorage().list: got[g.name] = g.value
+except:
+    pass
+lang = got["hl"]
 print("Content-type: text/html\n")
 print(temp["temp.html"](**{
     "root": "/" + folder + "/",
     "page": "main",
     "favicon": "/mahdi/Images/fav-icon.ico",
-    "title": "Mahdi Parastesh",
+    "title": {
+        "en": "Mahdi Parastesh",
+        "fa": "مهدی پرستش"
+    },
+    "header": {
+        "en": "Mahdi Parastesh",
+        "fa": "مهدی پرستش"
+    },
+    "biography": {
+        "en": """I have 6 years of experience in <b>Full-Stack software engineering</b>.<br><br>
+    The programming languages I'm skilled in are JavaScript, PHP, Java, SQL, Kotlin, Dart, Python and Prolog.
+    I can develop Android apps using mere Java/Kotlin, or also for iOS using Flutter or React Native.
+    I also know Node.js, TensorFlow, PyTorch, MySQL and PostgreSQL.<br>
+    I practice Machine Learning and I'm also an expert in Linux.""",
+        "fa": """"""
+    },
     "social": [
         {
             "icon": "github",
-            "name": "GitHub",
             "link": "https://github.com/fulcrum1378",
             "title": "fulcrum1378 (Mahdi Parastesh) · GitHub",
         },
         {
             "icon": "linkedin-in",
-            "name": "LinkedIn",
             "link": "https://www.linkedin.com/in/mahdi-parastesh-a72ab51b9/",
             "title": "Mahdi Parastesh | LinkedIn",
         },
         {
             "icon": "stack-overflow",
-            "name": "StackOverflow",
             "link": "https://stackoverflow.com/users/10728785/mahdi-parastesh",
             "title": "User Mahdi Parastesh - Stack Overflow",
         },
         {
             "icon": "facebook-f",
-            "name": "Facebook",
             "link": "https://www.facebook.com/mpg973",
             "title": "Mahdi Prs",
         },
         {
             "icon": "twitter",
-            "name": "Twitter",
             "link": "https://twitter.com/fulcrum1378",
             "title": "Mahdi Parastesh (@fulcrum1378) / Twitter",
         },
         {
             "icon": "instagram",
-            "name": "Instagram",
             "link": "https://www.instagram.com/fulcrum1378/",
             "title": "Mahdi Parastesh (@fulcrum1378) • Instagram photos and videos",
         },
@@ -51,8 +66,14 @@ print(temp["temp.html"](**{
     "projects": [
         {
             "id": "mergen",
-            "name": "Mergen",
-            "desc": "A logical Artificial Intelligence app, an operating system for a robot",
+            "name": {
+                "en": "Mergen",
+                "fa": "مِرگِن"
+            },
+            "desc": {
+                "en": "A logical Artificial Intelligence software, an operating system for a robot.",
+                "fa": "یک نرم افزار هوش مصنوعی منطقی، سیستم عاملی برای یک ربات."
+            },
             "source": "https://github.com/fulcrum1378/mergen_android",
         },
         # {
@@ -63,39 +84,75 @@ print(temp["temp.html"](**{
         # },
         {
             "id": "telexporter",
-            "name": "Telexporter",
-            "desc": "Export your messages to HTML, PDF or JSON files",
+            "name": {
+                "en": "Telexporter",
+                "fa": "تلکسپورتر"
+            },
+            "desc": {
+                "en": "Export your messages and call history to HTML, PDF or JSON files.",
+                "fa": "اس ام اس ها و تاریخچه تماس های خود را در قالب های وب، پی دی اف و جیسون استخراج کنید."
+            },
             "source": "https://github.com/fulcrum1378/telexporter",
         },
         {
             "id": "migratio",
-            "name": "Migratio",
-            "desc": "Migratio official Wordpress-powered website",
+            "name": {
+                "en": "Migratio",
+                "fa": "میگراتیو"
+            },
+            "desc": {
+                "en": "Migratio Wordpress-powered website",
+                "fa": "وبسایت وردپرسی میگراتیو"
+            },
             "source": "https://github.com/fulcrum1378/migratio",
         },
         {
             "id": "migratio",
-            "name": "Migratio (Android)",
-            "desc": "A geographical statistical tool for determining someone's best destination for migration",
+            "name": {
+                "en": "Migratio (Android)",
+                "fa": "میگراتیو (اندروید)"
+            },
+            "desc": {
+                "en": "A geographical statistical tool for determining someone's best destination for migration",
+                "fa": "یک ابزار جغرافی-آماری برای تعیین کردن بهترین مقصد مهاجرت افراد مختلف"
+            },
             "source": "https://github.com/fulcrum1378/migratio_android",
             "target": "https://migratio.mahdiparastesh.ir/",
         },
         {
             "id": "sexbook",
-            "name": "Sexbook",
-            "desc": "",
+            "name": {
+                "en": "Sexbook",
+                "fa": "سکسبوک"
+            },
+            "desc": {
+                "en": "Control your sexual life easily",
+                "fa": "زندگی جنسی خود را به آسانی کنترل کنید"
+            },
             "source": "https://github.com/fulcrum1378/sexbook",
         },
         {
             "id": "friend_tracker",
-            "name": "Friend Tracker",
-            "desc": "Friend Tracker, easily find your friends on the map",
+            "name": {
+                "en": "Friend Tracker",
+                "fa": "ردیاب رفقا"
+            },
+            "desc": {
+                "en": "Easily track your friends on the map.",
+                "fa": "رفقای خود را از روی نقشه ردیابی نمایید."
+            },
             "source": "https://github.com/fulcrum1378/friend_tracker"
         },
         {
             "id": "saam",
-            "name": "Saam",
-            "desc": "Stock Market Data Collector based upon MetaTrader5",
+            "name": {
+                "en": "Saam",
+                "fa": "سام"
+            },
+            "desc": {
+                "en": "Stock market data collector based on MetaTrader5",
+                "fa": "گرداور و ذخیره کننده اطلاعات بورس، ساخته شده بر مبنای متاتریدر 5"
+            },
             "source": "https://github.com/fulcrum1378/saam",
         },
     ],
