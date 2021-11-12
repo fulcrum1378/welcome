c0c66dac929bc690e5c4912f3f943ced72708f1c
Mahdi Parastesh <fulcrum1378@gmail.com>
2021-11-12 18:40:49 +0330

    2021.11.12 - 1

--- main.py
+++ main.py
@@ -1,38 +1,16 @@
-import cgi
 import os
 
 from chameleon import PageTemplateFile, PageTemplateLoader
-# Chameleon doesn't support Persian characters!
 
 folder = "welcome"
 temp = PageTemplateLoader(os.path.join(os.path.dirname(__file__), folder), encoding="utf-8")
-got = {"hl": "en"}
-try:
-    for g in cgi.FieldStorage().list: got[g.name] = g.value
-except:
-    pass
-l = got["hl"]
-print("Content-Type: text/html; charset=utf-8\n")
+# Chameleon doesn't support Persian characters!
+print("Content-Type: text/html\n")
 print(temp["temp.html"](**{
     "root": "/" + folder + "/",
     "page": "main",
     "favicon": "/mahdi/Images/fav-icon.ico",
-    "title": {
-        "en": "Mahdi Parastesh",
-        "fa": "مهدی پرستش"
-    }[l],
-    "header": {
-        "en": "Mahdi Parastesh",
-        "fa": "مهدی پرستش"
-    }[l],
-    # "biography": {
-    #    "en": """I have 6 years of experience in <b>Full-Stack software engineering</b>.<br><br>
-    # The programming languages I'm skilled in are JavaScript, PHP, Java, SQL, Kotlin, Dart, Python and Prolog.
-    # I can develop Android apps using mere Java/Kotlin, or also for iOS using Flutter or React Native.
-    # I also know Node.js, TensorFlow, PyTorch, MySQL and PostgreSQL.<br>
-    # I practice Machine Learning and I'm also an expert in Linux.""",
-    #    "fa": """"""
-    # }[l],
+    "title": "Mahdi Parastesh",
     "social": [
         {
             "icon": "github",
@@ -68,93 +46,51 @@ print(temp["temp.html"](**{
     "projects": [
         {
             "id": "mergen",
-            "name": {
-                "en": "Mergen",
-                "fa": "مِرگِن"
-            }[l],
-            "desc": {
-                "en": "A logical Artificial Intelligence software, an operating system for a robot.",
-                "fa": "یک نرم افزار هوش مصنوعی منطقی، سیستم عاملی برای یک ربات."
-            }[l],
+            "name": "Mergen",
+            "desc": "A logical Artificial Intelligence software, an operating system for a robot.",
             "source": "https://github.com/fulcrum1378/mergen_android",
         },
-        # {
-        #    "id": "avabot",
-        #    "name": "AvaBot",
-        #    "desc": "Speech Recognizer, Text-to-Speech and Optical Character Recognition for Persian language",
-        #    "source": "https://github.com/fulcrum1378/avabot-browser",
-        # },
+        {
+            "id": "mergen",
+            "name": "AvaBot",
+            "desc": "Speech Recognizer, Text-to-Speech and Optical Character Recognition for Persian language",
+            "source": "https://github.com/fulcrum1378/avabot-browser",
+        },
         {
             "id": "telexporter",
-            "name": {
-                "en": "Telexporter",
-                "fa": "تلکسپورتر"
-            }[l],
-            "desc": {
-                "en": "Export your messages and call history to HTML, PDF or JSON files.",
-                "fa": "اس ام اس ها و تاریخچه تماس های خود را در قالب های وب، پی دی اف و جیسون استخراج کنید."
-            }[l],
+            "name": "Telexporter",
+            "desc": "Export your messages and call history to HTML, PDF or JSON files.",
             "source": "https://github.com/fulcrum1378/telexporter",
         },
         {
             "id": "migratio",
-            "name": {
-                "en": "Migratio",
-                "fa": "میگراتیو"
-            }[l],
-            "desc": {
-                "en": "Migratio Wordpress-powered website",
-                "fa": "وبسایت وردپرسی میگراتیو"
-            }[l],
+            "name": "Migratio",
+            "desc": "Migratio Wordpress-powered website",
             "source": "https://github.com/fulcrum1378/migratio",
         },
         {
             "id": "migratio",
-            "name": {
-                "en": "Migratio (Android)",
-                "fa": "میگراتیو (اندروید)"
-            }[l],
-            "desc": {
-                "en": "A geographical statistical tool for determining someone's best destination for migration",
-                "fa": "یک ابزار جغرافی-آماری برای تعیین کردن بهترین مقصد مهاجرت افراد مختلف"
-            }[l],
+            "name": "Migratio (Android)",
+            "desc": "A geographical statistical tool for determining someone's best destination for migration.",
             "source": "https://github.com/fulcrum1378/migratio_android",
             "target": "https://migratio.mahdiparastesh.ir/",
         },
         {
             "id": "sexbook",
-            "name": {
-                "en": "Sexbook",
-                "fa": "سکسبوک"
-            }[l],
-            "desc": {
-                "en": "Control your sexual life easily",
-                "fa": "زندگی جنسی خود را به آسانی کنترل کنید"
-            }[l],
+            "name": "Sexbook",
+            "desc": "Control your sexual life easily.",
             "source": "https://github.com/fulcrum1378/sexbook",
         },
         {
             "id": "friend_tracker",
-            "name": {
-                "en": "Friend Tracker",
-                "fa": "ردیاب رفقا"
-            }[l],
-            "desc": {
-                "en": "Easily track your friends on the map.",
-                "fa": "رفقای خود را از روی نقشه ردیابی نمایید."
-            }[l],
+            "name": "Friend Tracker",
+            "desc": "Easily track your friends on the map.",
             "source": "https://github.com/fulcrum1378/friend_tracker"
         },
         {
             "id": "saam",
-            "name": {
-                "en": "Saam",
-                "fa": "سام"
-            }[l],
-            "desc": {
-                "en": "Stock market data collector based on MetaTrader5",
-                "fa": "گرداور و ذخیره کننده اطلاعات بورس، ساخته شده بر مبنای متاتریدر 5"
-            }[l],
+            "name": "Saam",
+            "desc": "Stock market data collector based on MetaTrader5",
             "source": "https://github.com/fulcrum1378/saam",
         },
     ],
--- welcome/main.js
+++ welcome/main.js
@@ -8,3 +8,38 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+// Translation
+var params = {}, rawParams = window.location.search.split("&");
+rawParams.forEach(function (p) {
+    let v = p.split("="), key = v[0];
+    if (key.charAt(0) == "?") key = key.substring(1);
+    params[key] = decodeURIComponent(v[1]);
+});
+function translate(hl) {
+    switch (hl) {
+        case "fa":
+            document.title = "مهدی پرستش";
+            $("p.mahdi").text("مهدی پرستش");
+            $("blockquote.mahdi").html('بنده مدت 6 سال در برنامه نویسی فولستک (بک اند + فرانت اند) و 3 سال در برنامه نویسی اندروید (با زبان های جاوا و کاتلین، بعلاوه فریمورک «فلاتر») تجربه دارم. از سال 1399 نیز به یادگیری سیستم عامل های لینوکسی (خصوصا فدورا، اوبونتو و سنتوس) و یونیکسی (FreeBSD و OpenBSD)، بعلاوه Windows Server و کسب تجربه در آنها مشغول شدم و در سیستم عامل جدید الورود شرکت هواوی (HarmonyOS) نیز کمی مهارت دارم. از سال 1400، الگوریتم های «هوش مصنوعی» را یاد گرفته و هم اکنون مشغول مطالعه و تمرین در این زمینه هستم.');
+            $("#cv").text("مشاهده رزومه من");
+            $(".mergen:eq(1) .projName").text("مِرگِن");
+            $(".mergen:eq(1) .projDesc").text("یک نرم افزار هوش مصنوعی منطقی، سیستم عاملی برای یک ربات.");
+            $(".mergen:eq(2) .projName").text("آوابات");
+            $(".mergen:eq(2) .projDesc").text("ابزار های تشخیص گفتار، تبدیل متن به گفتار و تشخیص متن از روی عکس برای زبان فارسی");
+            $(".telexporter .projName").text("تلکسپورتر");
+            $(".telexporter .projDesc").text("اس ام اس ها و تاریخچه تماس های خود را در قالب های وب، پی دی اف و جیسون استخراج کنید.");
+            $(".migratio:eq(1) .projName").text("میگراتیو");
+            $(".migratio:eq(1) .projDesc").text("وبسایت وردپرسی میگراتیو");
+            $(".migratio:eq(2) .projName").text("میگراتیو (اندروید)");
+            $(".migratio:eq(2) .projDesc").text("یک ابزار جغرافی-آماری برای تعیین کردن بهترین مقصد مهاجرت افراد مختلف.");
+            $(".sexbook .projName").text("سکسبوک");
+            $(".sexbook .projDesc").text("زندگی جنسی خود را به آسانی کنترل کنید.");
+            $(".friend_tracker .projName").text("ردیاب رفقا");
+            $(".friend_tracker .projDesc").text("رفقای خود را از روی نقشه ردیابی نمایید.");
+            $(".saam .projName").text("سام");
+            $(".saam .projDesc").text("گرداور و ذخیره کننده اطلاعات بورس، ساخته شده بر مبنای متاتریدر 5");
+            break;
+    }
+}
+if (params.hasOwnProperty("hl")) translate(params["hl"])
